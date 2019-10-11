5966c1a287b69edbeb1ef8a10057e25c42c934b8
ilyay95 <43435369+ilyay95@users.noreply.github.com>
2019-10-11 21:57:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let answer = 0;
+	let firstF,secondF;
+
+	firstF = expression.split('*');
+	firstF =firstF.filter(el => (el.slice(-2) !== '!!'));
+	firstF =firstF.map(el => parseInt(el,10));
+
+	secondF = expression.split('*');
+	secondF=secondF.filter(el => (el.slice(-2) === '!!'));
+	secondF=secondF.map(el => parseInt(el,10));
+  
+	function firstZeros(x) 
+	{
+		answer = answer + (Math.floor(x/5) + Math.floor(x/25));
+	} 
+  
+	function secondZeros(x) 
+	{
+	  	if (x%2 === 0) 
+	  	{
+			answer = answer + Math.floor(x/10) + Math.floor(x/50);
+		} 
+		  
+	 	else 
+		{
+			answer = answer + Math.floor(x/10);
+			if (x%10 >= 5) answer++;
+			if (x >= 25) answer++;
+			if (x >= 75) answer++;
+		}
+	}
+  
+	firstF.forEach(x => firstZeros(x));
+  
+	if  (secondF.some(el => el%2 === 0) || firstF.length) 
+	{
+		secondF.forEach(x => secondZeros(x));
+	}
+  
+	return answer;
+  }
+  
 }
